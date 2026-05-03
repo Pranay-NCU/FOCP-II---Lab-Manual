7a5fee41ddce5c5bb2761ba04ab56e36e1a8e8c8
Pranay <pranay25ecstu013@ncuindia.edu>
2026-05-03 20:20:08 +0530

    Create Q19.cpp

---
+++ Q19.cpp
@@ -0,0 +1,24 @@
+#include<iostream>
+using namespace std;
+
+int main() {
+    int a,b,i,j,f;
+
+    cin>>a>>b;
+
+    for(i=a;i<=b;i++) {
+        f=0;
+
+        for(j=2;j<i;j++) {
+            if(i%j==0) {
+                f=1;
+                break;
+            }
+        }
+
+        if(f==0 && i!=1)
+            cout<<i<<" ";
+    }
+
+    return 0;
+}
