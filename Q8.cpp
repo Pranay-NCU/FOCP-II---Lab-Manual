5b38cdb66903e2a869e6fd176674bd4fb5fd3e5c
Pranay <pranay25ecstu013@ncuindia.edu>
2026-05-03 20:13:41 +0530

    Create Q8.cpp

---
+++ Q8.cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+using namespace std;
+
+int main() {
+    int n,i;
+
+    cin>>n;
+
+    for(i=1;i<=n;i++) {
+        if(i%3==0 && i%5==0)
+            cout<<"BuzzFuzz"<<endl;
+        else if(i%3==0)
+            cout<<"Buzz"<<endl;
+        else if(i%5==0)
+            cout<<"Fuzz"<<endl;
+        else
+            cout<<i<<endl;
+    }
+
+    return 0;
+}
