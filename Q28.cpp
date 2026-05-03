9ca5eb4157908bbc6e5e83034c6decce2770c295
Pranay <pranay25ecstu013@ncuindia.edu>
2026-05-03 20:24:13 +0530

    Create Q28.cpp

---
+++ Q28.cpp
@@ -0,0 +1,22 @@
+#include<iostream>
+using namespace std;
+
+int main() {
+    int a[5],i,even=0,odd=0;
+
+    for(i=0;i<5;i++)
+        cin>>a[i];
+
+    for(i=0;i<5;i++) {
+
+        if(a[i]%2==0)
+            even=even+a[i];
+        else
+            odd=odd+a[i];
+    }
+
+    cout<<"Even Sum = "<<even<<endl;
+    cout<<"Odd Sum = "<<odd;
+
+    return 0;
+}
