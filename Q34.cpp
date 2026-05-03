3aa5975a27b210dfe83dc13f6fdb7cba69c61598
Pranay <pranay25ecstu013@ncuindia.edu>
2026-05-03 20:25:58 +0530

    Create Q34.cpp

---
+++ Q34.cpp
@@ -0,0 +1,34 @@
+#include<iostream>
+using namespace std;
+
+int main() {
+    int a[3][3],b[3][3],c[3][3];
+    int i,j;
+
+    for(i=0;i<3;i++) {
+        for(j=0;j<3;j++) {
+            cin>>a[i][j];
+        }
+    }
+
+    for(i=0;i<3;i++) {
+        for(j=0;j<3;j++) {
+            cin>>b[i][j];
+        }
+    }
+
+    for(i=0;i<3;i++) {
+        for(j=0;j<3;j++) {
+            c[i][j]=a[i][j]+b[i][j];
+        }
+    }
+
+    for(i=0;i<3;i++) {
+        for(j=0;j<3;j++) {
+            cout<<c[i][j]<<" ";
+        }
+        cout<<endl;
+    }
+
+    return 0;
+}
