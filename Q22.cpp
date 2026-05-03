dfb8043637b29d19f8d56c39500abdad60ae0ecd
Pranay <pranay25ecstu013@ncuindia.edu>
2026-05-03 20:21:35 +0530

    Create Q22.cpp

---
+++ Q22.cpp
@@ -0,0 +1,40 @@
+#include<iostream>
+using namespace std;
+
+int main() {
+    int i,j,s;
+
+    for(i=1;i<=4;i++) {
+
+        for(s=1;s<=4-i;s++)
+            cout<<" ";
+
+        for(j=1;j<=2*i-1;j++) {
+
+            if(j==1 || j==2*i-1)
+                cout<<"*";
+            else
+                cout<<" ";
+        }
+
+        cout<<endl;
+    }
+
+    for(i=3;i>=1;i--) {
+
+        for(s=1;s<=4-i;s++)
+            cout<<" ";
+
+        for(j=1;j<=2*i-1;j++) {
+
+            if(j==1 || j==2*i-1)
+                cout<<"*";
+            else
+                cout<<" ";
+        }
+
+        cout<<endl;
+    }
+
+    return 0;
+}
