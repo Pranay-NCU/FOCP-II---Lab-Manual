f2723d953e548afce618c3959ee8e41d066f30e0
Pranay <pranay25ecstu013@ncuindia.edu>
2026-05-03 20:16:35 +0530

    Create Q16.cpp

---
+++ Q16.cpp
@@ -0,0 +1,22 @@
+#include<iostream>
+using namespace std;
+
+int main() {
+    int n,i,f=0;
+
+    cin>>n;
+
+    for(i=2;i<n;i++) {
+        if(n%i==0) {
+            f=1;
+            break;
+        }
+    }
+
+    if(f==0)
+        cout<<"Prime";
+    else
+        cout<<"Not Prime";
+
+    return 0;
+}
