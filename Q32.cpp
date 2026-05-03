5b3e74a65952bab7dc0dd340a09ff96154071933
Pranay <pranay25ecstu013@ncuindia.edu>
2026-05-03 20:25:29 +0530

    Create Q32.cpp

---
+++ Q32.cpp
@@ -0,0 +1,27 @@
+#include<iostream>
+using namespace std;
+
+int main() {
+    int a[5],i,largest,second;
+
+    for(i=0;i<5;i++)
+        cin>>a[i];
+
+    largest=second=a[0];
+
+    for(i=1;i<5;i++) {
+
+        if(a[i]>largest) {
+            second=largest;
+            largest=a[i];
+        }
+        else if(a[i]>second && a[i]!=largest) {
+            second=a[i];
+        }
+    }
+
+    cout<<"Largest = "<<largest<<endl;
+    cout<<"Second Largest = "<<second;
+
+    return 0;
+}
